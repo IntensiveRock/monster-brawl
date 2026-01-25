2780b450235c433b0ba60871e0a8e6d4ba161f56
IntensiveRock <froggycham@gmail.com>
2026-01-25 10:47:25 -0700

    deck images

--- scripts/createdeckimages.py
+++ scripts/createdeckimages.py
@@ -3,6 +3,7 @@ After running the deckbuilder, construct the card images.
 """
 import click
 from pathlib import Path
+from PIL import Image
 
 from monster_brawl.db import grab_cards_from_db
 from monster_brawl.card import MonsterCard, GearCard, SpellCard
@@ -15,8 +16,9 @@ def db_to_images(name, deckpath):
     monsters, mcols = grab_cards_from_db(deckpath, "monsters")
     gear, gcols = grab_cards_from_db(deckpath, "gear")
     spells, scols = grab_cards_from_db(deckpath, "spells")
-    card_dir = Path(name)
-    card_dir.mkdir(exist_ok=True)
+    # card_dir = Path(name)
+    # card_dir.mkdir(exist_ok=True)
+    card_list = []
     for index, row in enumerate(monsters):
         try:
             tmp_monster = MonsterCard(
@@ -27,8 +29,9 @@ def db_to_images(name, deckpath):
                 hp=row[4],
                 atk=row[5],)
             img = tmp_monster.draw()
-            cpath = Path(card_dir, f"m{index}.png")
-            img.save(cpath)
+            card_list.append(img)
+            # cpath = Path(card_dir, f"m{index}.png")
+            # img.save(cpath)
         except:
             pass
     for index, row in enumerate(gear):
@@ -40,8 +43,9 @@ def db_to_images(name, deckpath):
                 rank=row[3],
                 cost=row[4])
             img = tmp_monster.draw()
-            cpath = Path(card_dir, f"g{index}.png")
-            img.save(cpath)
+            card_list.append(img)
+            # cpath = Path(card_dir, f"g{index}.png")
+            # img.save(cpath)
         except:
             pass
     for index, row in enumerate(spells):
@@ -51,11 +55,21 @@ def db_to_images(name, deckpath):
                 desc=row[1],
                 cost=row[2])
             img = tmp_monster.draw()
-            cpath = Path(card_dir, f"s{index}.png")
-            img.save(cpath)
+            card_list.append(img)
+            # cpath = Path(card_dir, f"s{index}.png")
+            # img.save(cpath)
         except:
             pass
+    deck_grid = image_grid(card_list, 10, 7)
+    deck_grid.save(f"{name}.png")
 
+def image_grid(imgs, rows, cols):
+    w, h = imgs[0].size
+    grid = Image.new('RGB', size=(cols*w, rows*h))
+    grid_w, grid_h = grid.size
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i%cols*w, i//cols*h))
+    return grid
 
 
 if __name__ == "__main__":
